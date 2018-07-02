a2406e195ee8b8e60601ce075248f4b6fcd284aa
Jérôme V <jeromevvb@gmail.com>
2018-07-02 11:17:59 +0200

    add form reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { mixerStore } from "react-arc";
 import { applyMiddleware, createStore, combineReducers, compose } from "redux";
 import thunk from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 
 import configCityWeather from "./../cityWeather/config/reactArc";
 import citiesReducer from "./reducers/citiesReducer";
@@ -11,7 +12,8 @@ const middleware = applyMiddleware(thunk);
 const reducers = {
   viewWeather: mixerStore({ config: configCityWeather }),
   citiesReducer: citiesReducer,
-  cityWeatherDetail: cityWeatherDetail
+  cityWeatherDetail: cityWeatherDetail,
+  form: formReducer
 };
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
