edbaba6ff89f340d67c6b0d63f2f5b3aa4d24de7
Jérôme V <jeromevvb@gmail.com>
2018-07-02 14:56:45 +0200

    get onSubmit from redux-form

--- src/searchBar/SearchBarContainer.js
+++ src/searchBar/SearchBarContainer.js
@@ -6,15 +6,19 @@ import SearchBarForm from "./components/SearchBarForm";
 import { addCity } from "../store/actions/citiesActions";
 
 class SearchBarContainer extends Component {
+  onSubmit = values => {
+    console.log(values);
+  };
+
   render() {
     return (
       <div>
-        <SearchBarForm
-          onSubmitCity={city => this.props.dispatch(addCity(city))}
-        />
+        <SearchBarForm onSubmit={this.onSubmit} />
       </div>
     );
   }
 }
 
 export default withRouter(connect()(SearchBarContainer));
+
+/* onSubmit={city => this.props.dispatch(addCity(city))} */
